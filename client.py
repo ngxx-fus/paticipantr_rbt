2a8da758bd5f083f99173e877787273cd54517c2
fus <nguyenphu3204@outlook.com>
2023-11-24 22:11:38 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,27 +1,60 @@
-import socket       
-import sys      
-import time
+# Import socket module
+import socket
 import cv2
 import numpy as np
+import torch
+import time
 import json
 import base64
+import os
 
-# Create a socket object 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+global sendBack_angle, sendBack_Speed, current_speed, current_angle
+sendBack_angle = 0
+sendBack_Speed = 0
+current_speed = 0
+current_angle = 0
 
-# Define the port on which you want to connect 
-port = 54321                
-  
-# connect to the server on local computer 
-s.connect(('127.0.0.1', port)) 
 
-count = 0
-angle = 10
-speed = 100
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the port on which you want to connect
+PORT = 54321
+# connect to the server on local computer
+s.connect(('127.0.0.1', PORT))
+
+def Control(angle, speed):
+    global sendBack_angle, sendBack_Speed
+    sendBack_angle = angle
+    sendBack_Speed = speed
+
+#-----------------PID Controller-------------------#
+error_arr = np.zeros(5)
+pre_t = time.time()
+MAX_SPEED = 60
+
+def PID(error, p, i, d):
+    global pre_t
+    # global error_arr
+    error_arr[1:] = error_arr[0:-1]
+    error_arr[0] = error
+    P = error*p
+    delta_t = time.time() - pre_t
+    #print('DELAY: {:.6f}s'.format(delta_t))
+    pre_t = time.time()
+    D = (error-error_arr[1])/delta_t*d
+    I = np.sum(error_arr)*delta_t*i
+    angle = P + I + D
+    if abs(angle)>25:
+        angle = np.sign(angle)*25
+    return int(angle)
+
+
 if __name__ == "__main__":
     try:
-        """
-            - Chương trình đưa cho bạn 3 giá trị đầu vào:
+        while True:
+            """
+            - Chương trình đưa cho bạn 1 giá trị đầu vào:
                 * image: hình ảnh trả về từ xe
                 * current_speed: vận tốc hiện tại của xe
                 * current_angle: góc bẻ lái hiện tại của xe
@@ -31,39 +64,40 @@ if __name__ == "__main__":
                 Trong đó:
                     + sendBack_angle (góc điều khiển): [-25, 25]
                         NOTE: ( âm là góc trái, dương là góc phải)
-                    + sendBack_Speed (tốc độ điều khiển): [0, 150]
+                    + sendBack_Speed (tốc độ điều khiển): [-150, 150]
+                        NOTE: (âm là lùi, dương là tiến)
             """
-        while True:
-            # Gửi góc lái và tốc độ để điều khiển xe
-            message = bytes(f"{angle} {speed}", "utf-8")
+
+            message = bytes(f"{sendBack_angle} {sendBack_Speed}", "utf-8")
             s.sendall(message)
-
-            # Recive data from server
             data = s.recv(100000)
-            # print(data)
+            
             data_recv = json.loads(data)
-
-            # Angle and speed recv from server
             current_angle = data_recv["Angle"]
             current_speed = data_recv["Speed"]
-            print("angle: ", current_angle)
-            print("speed: ", current_speed)
-            print("---------------------------------------")
-            #Img data recv from server
             jpg_original = base64.b64decode(data_recv["Img"])
             jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
-            imgage = cv2.imdecode(jpg_as_np, flags=1)
-            cv2.imshow("IMG", imgage)
-            print("Img Shape: ",imgage.shape)
+            image = cv2.imdecode(jpg_as_np, flags=1)
+
+            print(current_speed, current_angle)
+            print(image.shape)            
+           
+
+            """ Display images """
+            cv2.imshow('Image Original', image)
             # save image
+            count = 0
+            if os.path.exists("./img")==False:
+                os.makedirs("./img")
             image_name = "./img/img_{}.jpg".format(count)
             count += 1
-            cv2.imwrite(image_name, imgage)
-            key = cv2.waitKey(1)
-        
-            
+            cv2.imwrite(image_name, image)
+
+            Control(sendBack_angle, sendBack_Speed)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     finally:
         print('closing socket')
         s.close()
-
